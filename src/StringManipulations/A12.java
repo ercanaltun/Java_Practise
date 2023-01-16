8b9270c8ae921dcf04fe207abd9184af1fbac2a5
Ercan ALTUN <salsel0147@gmail.com>
2023-01-16 11:57:50 +0300

    16.01.2023-3

---
+++ src/StringManipulations/A12.java
@@ -0,0 +1,12 @@
+package StringManipulations;
+
+public class A12 {
+
+      /*
+      Problem Tanımı :
+      Basit 4 islem yapan bir hesap makinesi kodlayiniz....
+      Kullanicidan yapacagi islemi islem sembolu ile secmesini saglayalinız.
+      Kullanicidan iki sayi girmesini isteyiniz.
+      Girilen iki sayi ve secilen isleme gore dogru sonucu ekrana yazdirınız.
+    */
+}
---
+++ src/StringManipulations/A14.java
@@ -0,0 +1,10 @@
+package StringManipulations;
+
+public class A14 {
+
+    //Kullanicidan iki keline isteyin.İki kelimeyi
+    //(ilk kelime yarısı+ikinci kelime+ilk kelimenin ikinci yarısı)
+    //şeklinde birleştirin.ilk kelime tekse
+    // "1.kelimeyi uzunluğu çift sayı girin"uyarısı versin
+
+}
