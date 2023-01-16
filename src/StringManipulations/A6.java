2781c4c0c1faf9b335914da3d5a2800d324af32e
Ercan ALTUN <salsel0147@gmail.com>
2023-01-16 11:46:12 +0300

    16.01.2023

---
+++ src/StringManipulations/A6.java
@@ -0,0 +1,14 @@
+package StringManipulations;
+
+public class A6 {
+
+    /*
+     * girilen String icinde "xyz" dizimi var ise true degilse false
+     *  return eden metod yaziniz
+     * input      output
+     * axyzm  ==   true
+     * xyz    ==   true
+     * x.yz   ==   false
+     * xyaz   ==   false
+     */
+}
---
+++ src/StringManipulations/A8.java
@@ -0,0 +1,19 @@
+package StringManipulations;
+
+public class A8 {
+
+            /*
+         Kullanicidan bir kelime girmesini isteyin.
+         * Sozcukte tek sayida karakter ve 3 veya daha fazla karakter iceriyorsa,
+         * kelimenin ortasindaki karakteri yazdirin.
+
+
+                Scanner scan=new Scanner(System.in);
+        System.out.println("bir kelime giriniz");
+        String str=scan.next();
+        char ortancaKarakter=str.charAt((str.length()-1)/2);
+        if (str.length()%2==1 && str.length()>=3){
+            System.out.println("ortancaKarakter = " + ortancaKarakter);
+        }
+        */
+}
