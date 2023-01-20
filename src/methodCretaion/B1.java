ec4a9d8d91b78360fa409566ccef725873baad91
Ercan ALTUN <salsel0147@gmail.com>
2023-01-20 12:50:07 +0300

    20.01.2023

---
+++ src/methodCretaion/B1.java
@@ -0,0 +1,16 @@
+package methodCretaion;
+
+public class B1 {
+
+    public static  void main(String [] args){
+
+    /*
+    Soru 1: cm olarak verilen bir sayiyi, metre ve kilometreye donusturen bir method yaziniz
+      */
+
+
+
+    }
+
+
+}
