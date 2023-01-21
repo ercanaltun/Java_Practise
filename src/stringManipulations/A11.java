7c00892fbc77b9e932506838b15f52796a007d4d
Ercan ALTUN <salsel0147@gmail.com>
2023-01-21 14:41:42 +0300

    21.01.2023

--- src/stringManipulations/A11.java
+++ src/stringManipulations/A11.java
@@ -1,5 +1,7 @@
 package stringManipulations;
 
+import java.util.Scanner;
+
 public class A11 {
 
     // Kullanicidan 4 harfli bir kelime isteyin
@@ -8,6 +10,17 @@ public class A11 {
 
     public static  void main(String [] args){
 
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Please entera four letter string");
+        String input=scan.next();
+        String backwardWord=input.substring(input.length()-1);
+        for(int i=input.length()-2;i>=0;i--)
+            backwardWord+=input.substring(i,i+1);
+
+
+        System.out.println("backwardWord = " + backwardWord);
+
 
     }
+
 }
--- src/stringManipulations/A12.java
+++ src/stringManipulations/A12.java
@@ -1,5 +1,7 @@
 package stringManipulations;
 
+import java.util.Scanner;
+
 public class A12 {
 
       /*
@@ -11,7 +13,37 @@ public class A12 {
     */
 
     public static  void main(String [] args){
+        Scanner scan=new Scanner(System.in);
+        System.out.println("enter kind of math's mark");
+        int mathMark=scan.nextInt();
+        System.out.println("enter first number");
+        double num1= scan.nextInt();
+        System.out.println("enter second number");
+        double num2=scan.nextInt();
+        
+        fourMathsDo(num1,num2,mathMark);
 
 
+        }
+
+    private static void fourMathsDo(double num1, double num2, int mathMark) {
+        
+        
+        switch (mathMark){
+            
+            case 1 : System.out.println("sum of numbers = " + (num1+num2));break;
+            case 2 : double subtract=num1-num2; System.out.println("subtract of numbers = "+subtract);break;
+            case 3 : System.out.println("multiply of numbers = " +num1*num2);break;
+            case 4 : double divide=num1/num2; System.out.println("divide of numbers = "+divide);break;
+            default: System.out.println("Please a correct numeric value for maths");
+
+
+        }
+        
+        
+        
+        
     }
+    
 }
+
